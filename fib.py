64cc7aee9afc944e63cf6cb8cb3034c700869f79
Ramon Alonzo <ramon.alonzo@scouting.org>
2019-04-10 15:15:05 -0500

    Refactor to create a function

--- fib.py
+++ fib.py
@@ -1,10 +1,16 @@
 
 import sys, math
 
-S = int((sys.argv[1]))
-
 fib = [0, 1]
 
-for i in range(0,S-2,1):
-	fib.append(fib[i] + fib[i+1])
+def fun(N):
+	for i in range(0,S-2,1):
+		fib.append(fib[i] + fib[i+1])
+
+S = int((sys.argv[1]))
+fun(S)
 print fib
+
+
+
+
